b3d7404cb1797c8bdd5f28062b688e03ffb44ef3
safemall <ifechukwuezekwem47@gmail.com>
2025-04-30 05:18:10 -0700

    Update settings.py

--- safemall_api/settings.py
+++ safemall_api/settings.py
@@ -104,7 +104,7 @@ CACHES = {
 
 if os.environ.get("DATABASE_URL"):
     DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 else:
     DATABASES = {
