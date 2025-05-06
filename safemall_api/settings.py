56641bd865760d04bf6d252e50704a1d7b4494de
safemall <ifechukwuezekwem47@gmail.com>
2025-05-06 08:25:59 -0700

    Update settings.py

--- safemall_api/settings.py
+++ safemall_api/settings.py
@@ -102,18 +102,28 @@ CACHES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-   
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+
+
+# if os.environ.get("DATABASE_URL"):
+#     DATABASES = {
+#         'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+#     }
+# else:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+   
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
