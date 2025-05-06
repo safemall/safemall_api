fcd3ca5c2abe967d61e9501aea4930d57b65f137
safemall <ifechukwuezekwem47@gmail.com>
2025-05-06 09:24:28 -0700

    Update settings.py

--- safemall_api/settings.py
+++ safemall_api/settings.py
@@ -102,16 +102,11 @@ CACHES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # if os.environ.get("DATABASE_URL"):
