bdc74a3a54f76a47ce8eb74f2bb2c62dbf7b2e33
safemall <ifechukwuezekwem47@gmail.com>
2025-05-08 01:58:54 -0700

    Update consumers.py

--- restapi/consumers.py
+++ restapi/consumers.py
@@ -24,6 +24,7 @@ class ChatConsumer(WebsocketConsumer):
 
 
     def disconnect(self, close_code):
+        pass
         # Leave room group
         # async_to_sync(self.channel_layer.group_discard)(
         #     self.room_group_name,
