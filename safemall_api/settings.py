eff746b7e8bc3333f89951bb1dae69e67832b708
safemall <ifechukwuezekwem47@gmail.com>
2025-05-08 15:40:03 -0700

    Update settings.py

--- safemall_api/settings.py
+++ safemall_api/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'restapi.middleware.ActiveUserCacheMiddleware',
+    'restapi.middleware.ActiveUserCacheMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
